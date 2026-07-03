43543fce39c4ad1bbe6adec03d7f3671b71aac7e
Mr-p1gnum <andruyha5000@gmail.com>
2026-07-04 01:25:35 +0400

    Растасовал файлы для nginx, при наличии данных в .env пишется допю строка

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,19 @@
+import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HEADER = os.environ.get("HEADER", "")
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
+            body = "Hello from Effective Mobile!"
+            if HEADER:
+                body += f"\n{HEADER}"
+
             self.send_response(200)
             self.send_header("Content-Type", "text/plain; charset=utf-8")
             self.end_headers()
-            self.wfile.write("Hello from Effective Mobile!".encode("utf-8"))
+            self.wfile.write(body.encode("utf-8"))
         else:
             self.send_response(404)
             self.send_header("Content-Type", "text/plain; charset=utf-8")
@@ -14,7 +21,6 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write("Not Found".encode("utf-8"))
 
     def log_message(self, format, *args):
-        # можно оставить дефолтное логирование или убрать
         pass
 
 if __name__ == "__main__":
